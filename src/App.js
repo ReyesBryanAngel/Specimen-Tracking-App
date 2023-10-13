72ca88117e2f489d67cb35dc3f051191a5b7dc05
Angel Bryan Reyes <reyesangelbryan@gmail.com>
2023-10-13 13:39:01 +0800

    feat: manaully input courier information

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SpecimenReview from "./components/specimen-form/SpecimenReview";
 import Courier from "./components/records/Courier";
 import Patients from "./components/records/Patients";
 import ReviewSamples from "./components/records/ReviewSamples"
+import CourierInformation from "./components/courier/CourierInformation";
 
 function App() {
   const location = useLocation();
@@ -29,6 +30,9 @@ function App() {
           <Route path="/courier" element={<Courier/>} />
           <Route path="/patients" element={<Patients/>} />
           <Route path="/review-samples" element={<ReviewSamples/>} />
+          <Route path="/review-samples" element={<ReviewSamples/>} />
+          <Route path="/courier-information" element={<CourierInformation/>} />
+
           <Route path="/add-specimen/*">
             <Route index element={<ScanBarcode />} />
             <Route path="specimen-form" element={<SpecimenForm />} />
